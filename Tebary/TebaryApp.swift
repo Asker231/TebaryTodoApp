c595f23fdba890a6f56b6762a5e625866eb4eff2
Asker <kotsevasker0@gmail.com>
2023-05-09 20:08:31 +0300

    Initial Commit

---
+++ Tebary/TebaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TebaryApp.swift
+//  Tebary
+//
+//  Created by ASKER on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TebaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
